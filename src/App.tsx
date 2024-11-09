a7d0fe633759c6faa27f8b4f011c1cfa11860ffa
qty0 <peterszabototh@gmail.com>
2024-11-09 14:43:02 +0000

    x

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
-import './App.scss'
-import '@styles/examplePage.scss'
+// import { useStateTogether } from 'react-together'
 
-import { useStateTogether } from 'react-together'
-
-import { version } from '@package'
-import { HeroLogo } from '@components'
+import RestaurantList from '@pages/RestaurantList'
+import './index.css'
 
 export default function App() {
-  const [count, set_count] = useStateTogether('counter_0', 0)
+  // const [count, set_count] = useStateTogether('counter_0', 0)
 
   return (
-    <div>
-      <div>
-        <HeroLogo {...{ type: 'reacttogether' }} />
-        <HeroLogo {...{ type: 'react' }} />
-        <HeroLogo {...{ type: 'vite' }} />
-      </div>
-      <h1>ReactTogether + Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => set_count((count) => count + 1) }>Synq'd count is {count}</button> 
-        {/* <button onClick={() => set_count((count) => count + 1)}>Synq'd count is {count}</button> */}
-        <button {...{ style: { marginLeft: '1rem' }, onClick: () => set_count(0) }}>Reset</button>
-      </div>
-      <p className='read-the-docs'>Click on the respective logos to learn more.</p>
-
-      <div className='version-num'>{version}</div>
+    <div className='min-h-screen min-w-screen flex'>
+      <RestaurantList />
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import ViteYaml from '@modyfi/vite-plugin-yaml'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
-  plugins: [
-    ViteYaml(),
-    react(),
-    tsconfigPaths(),
-  ]
+  css: {
+    postcss: {
+      plugins: [tailwind, autoprefixer],
+    },
+  },
+  plugins: [ViteYaml(), react(), tsconfigPaths()],
 })
